dfc113e737ef66082e1b20eab71941c96911bc11
Anjalo Hettiarachchi <24694418+AnjaloHettiarachchi@users.noreply.github.com>
2021-06-27 18:54:20 +0530

    Refactor old implementation
    Signed-off-by: Anjalo Hettiarachchi <24694418+AnjaloHettiarachchi@users.noreply.github.com>

--- employee-manager-web/src/components/EmployeeTable.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, Heading } from '@chakra-ui/react';
-
-export const EmployeeTable = () => {
-  return (
-    <Box height="100%">
-      <Heading size={'xl'}>Table here.</Heading>
-    </Box>
-  );
-};
